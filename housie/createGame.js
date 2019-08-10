4c540288bd696270847d45b2cb4246ed0fadd03f
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2019-08-10 20:52:17 +0530

    add draw sequesnce

--- housie/createGame.js
+++ housie/createGame.js
@@ -1,4 +1,6 @@
 const {createGame} = require("./game_service");
+const tambola = require('tambola-generator');
+
 const game = {
   "status": "DRAFTED",
   "name": "Away day 2019",
@@ -16,6 +18,7 @@ const game = {
 
 
 (async () => {
+  game.sequence = tambola.getDrawSequence();
   const created = await createGame(game);
   console.log(created.result);
   return;
