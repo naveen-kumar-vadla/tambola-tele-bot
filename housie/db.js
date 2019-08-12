05152a7185b5f954d49f9a6ba1a802d762babc23
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2019-08-12 17:44:02 +0530

    Fix multiple connections issue

--- housie/db.js
+++ housie/db.js
@@ -1,38 +1,39 @@
 const MongoClient = require('mongodb').MongoClient;
 const {database} = require("../config");
 const COLLECTION_NAME = "housie";
+let db;
 
-const insert = async (document) => {
-  const db = await connection();
-  const collection = db.collection(COLLECTION_NAME);
-  return await collection.insertOne(document);
-};
-
-const find = async () => {
-  const db = await connection();
-  const collection = db.collection(COLLECTION_NAME);
-  return collection.findOne();
-};
-
-const update = async (game) => {
-  const db = await connection();
-  const collection = db.collection(COLLECTION_NAME);
-  return collection.updateOne({_id: game._id}, {$set: game});
-};
-
-const remove = async () => {
-  const db = await connection();
-  const collection = db.collection(COLLECTION_NAME);
-  const game = await find();
-  return collection.deleteOne({_id: game._id});
-};
-
-//private
 const connection = () => {
   return MongoClient.connect(process.env.DATABASE_URL || database.url, {useNewUrlParser: true})
       .then(client => client.db(database.name))
       .catch(err => console.log("Error: ", err));
 };
 
+(async () => {
+  console.log("Wait until DB gets initialized....");
+  db = await connection();
+  console.log("DB initialized... continue...");
+})();
+
+const insert = async (document) => {
+  const collection = db.collection(COLLECTION_NAME);
+  return await collection.insertOne(document);
+};
+
+const find = async () => {
+  const collection = db.collection(COLLECTION_NAME);
+  return collection.findOne();
+};
+
+const update = async (game) => {
+  const collection = db.collection(COLLECTION_NAME);
+  return collection.updateOne({_id: game._id}, {$set: game});
+};
+
+const remove = async () => {
+  const collection = db.collection(COLLECTION_NAME);
+  const game = await find(db);
+  return collection.deleteOne({_id: game._id});
+};
 
 module.exports = {insert, find, update, remove};
